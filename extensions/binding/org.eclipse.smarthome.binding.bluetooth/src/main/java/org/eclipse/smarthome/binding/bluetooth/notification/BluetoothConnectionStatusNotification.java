27c5a4fd6d4fcdf28db25204d1cffa8b6f2223b3
Kai Kreuzer <kai@openhab.org>
2017-11-07 20:35:05 +0100

    refactored namespace from ble to bluetooth
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

---
+++ extensions/binding/org.eclipse.smarthome.binding.bluetooth/src/main/java/org/eclipse/smarthome/binding/bluetooth/notification/BluetoothConnectionStatusNotification.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2010-2017 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.smarthome.binding.bluetooth.notification;
+
+import org.eclipse.smarthome.binding.bluetooth.BluetoothDevice.ConnectionState;
+
+/**
+ * The {@link BluetoothConnectionStatusNotification} provides a notification of a change in the device connection state.
+ *
+ * @author Chris Jackson - Initial contribution
+ */
+public class BluetoothConnectionStatusNotification extends BluetoothNotification {
+    private ConnectionState connectionState;
+
+    public BluetoothConnectionStatusNotification(ConnectionState connectionState) {
+        this.connectionState = connectionState;
+    }
+
+    /**
+     * Returns the connection state for this notification
+     * 
+     * @return the {@link ConnectionState}
+     */
+    public ConnectionState getConnectionState() {
+        return connectionState;
+    };
+}
