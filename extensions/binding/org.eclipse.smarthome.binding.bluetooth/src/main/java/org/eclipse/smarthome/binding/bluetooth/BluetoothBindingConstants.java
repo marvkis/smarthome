e4cc519b370f514a72b04d296e7edaa5dd6404cb
Kai Kreuzer <kai@openhab.org>
2017-11-14 12:05:50 +0100

    added constants about Bluetooth classes and profiles
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

--- extensions/binding/org.eclipse.smarthome.binding.bluetooth/src/main/java/org/eclipse/smarthome/binding/bluetooth/BluetoothBindingConstants.java
+++ extensions/binding/org.eclipse.smarthome.binding.bluetooth/src/main/java/org/eclipse/smarthome/binding/bluetooth/BluetoothBindingConstants.java
@@ -7,6 +7,8 @@
  */
 package org.eclipse.smarthome.binding.bluetooth;
 
+import java.util.UUID;
+
 import org.eclipse.smarthome.core.thing.ThingTypeUID;
 
 /**
@@ -33,5 +35,17 @@ public class BluetoothBindingConstants {
     public static final String CONFIGURATION_ADDRESS = "ble_address";
 
     public static final long bleUuid = 0x800000805f9b34fbL;
+
+    // Bluetooth profile UUID definitions
+    public final static UUID PROFILE_GATT = UUID.fromString("00001801-0000-1000-8000-00805f9b34fb");
+    public final static UUID PROFILE_A2DP_SOURCE = UUID.fromString("0000110a-0000-1000-8000-00805f9b34fb");
+    public final static UUID PROFILE_A2DP_SINK = UUID.fromString("0000110b-0000-1000-8000-00805f9b34fb");
+    public final static UUID PROFILE_A2DP = UUID.fromString("0000110d-0000-1000-8000-00805f9b34fb");
+    public final static UUID PROFILE_AVRCP_REMOTE = UUID.fromString("0000110c-0000-1000-8000-00805f9b34fb");
+    public final static UUID PROFILE_CORDLESS_TELEPHONE = UUID.fromString("00001109-0000-1000-8000-00805f9b34fb");
+    public final static UUID PROFILE_DID_PNPINFO = UUID.fromString("00001200-0000-1000-8000-00805f9b34fb");
+    public final static UUID PROFILE_HEADSET = UUID.fromString("00001108-0000-1000-8000-00805f9b34fb");
+    public final static UUID PROFILE_HFP = UUID.fromString("0000111e-0000-1000-8000-00805f9b34fb");
+    public final static UUID PROFILE_HFP_AUDIOGATEWAY = UUID.fromString("0000111f-0000-1000-8000-00805f9b34fb");
 
 }
